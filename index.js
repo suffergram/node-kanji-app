6730e51263d00454a13ff2c2ce37f587bd6dda64
petrushinmd <petrushinmd@ya.ru>
2024-02-11 00:00:42 +0300

    initial configuration

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const express = require('express');
+const fs = require('fs').promises;
+
 const app = express();
 const port = 4000;
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
